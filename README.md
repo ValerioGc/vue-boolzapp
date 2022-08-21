75d4494326290c4b082844d55a065f7a1a12e2a9
ValerioGc <valerio.gc1@gmail.com>
2022-08-22 00:03:41 +0200

    Revisione

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# __Exercise Vue-Boolzapp__
+#### *Replica Webapp messaggistica* 
+***************************
+
+<br/>
+
+
+## *_Used Languages:_*
+<p>
+    <img align="center" src="https://upload.wikimedia.org/wikipedia/commons/8/82/Devicon-html5-plain.svg" width="36" height="36" alt="HTML5" />
+    <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer">HTML 5</a>     
+</p>
+<p>
+    <img align="center" src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg" width="36" height="36" align="center" alt="CSS3" />
+    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noreferrer">CSS 3</a>
+</p>
+<p>    
+    <img align="center" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" width="36" height="36" alt="JavaScript" />
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">JavaScript ES6</a>
+</p>
+
+<br/>
+
+
+## *_Used Framework_*:
+
+<p>
+    <img align="center"src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vuejs-colored.svg" width="36" height="36" alt="Vue" />
+    <a href="https://vuejs.org/" target="_blank" rel="noreferrer">Vue Js</a>
+</p>
+
+<br/>
+
+## *_Used Libraries / API_*:
+
+<p>
+    <img align="center"src="https://cdn.worldvectorlogo.com/logos/fontawesome-1.svg" width="36" height="36" alt="Font-Awesome" />
+    <a href="https://vuejs.org/" target="_blank" rel="noreferrer">Font-Awersome</a>
+</p>
+<p>
+    <img align="center"src="https://raw.githubusercontent.com/moment/luxon/96994411ae941ce4f2c6aeff55d6f2ac9c21d908/site/docs/_media/Luxon_icon.svg" width="36" height="36" alt="Luxon 3.x" />
+    <a href="https://moment.github.io/luxon/#/" target="_blank" rel="noreferrer">Luxon 3</a>
+</p>
--- js/main.js
+++ js/main.js
@@ -2,23 +2,27 @@ var DateTime = luxon.DateTime;
 // App Vue
 var app = new Vue({
     el: '#container-app',
-    data: {    
+    data: {
     // Controllo larghezza schermo
         screenType: 'desktop',
-        // screenWidth: window.screen.width,
     // Definizione mittente messaggio
         isSent: '',
+        isHidden: true,
+        isHiddenHD: true,
     // Ora e Data
         lastMessageDate: '',
         lastMessageTime: '',
         lastMessage: '',
     // Chat Attiva
+        chatEmpty: false,
         activeChatIndex: 0,
     // Nuovo Messaggio
         newMessage:'',
         txtInput: '',
+    // Input Ricerca
         searchInput: '',
     // Contatti
+        noCnt: false,
         contacts: [
             {
                 name: 'Michele',
@@ -48,7 +52,7 @@ var app = new Vue({
                 name: 'Fabio',
                 avatar: '_2',
                 messages: [
-                    { 
+                    {
                         date: '20/03/2020 ',
                         hour:' 16:30:00',
                         message: 'Ciao come stai?',
@@ -73,19 +77,19 @@ var app = new Vue({
                 avatar: '_3',
                 messages: [
                     {
-                        date: '28/03/2020 ', 
+                        date: '28/03/2020 ',
                         hour:' 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '28/03/2020 ', 
+                        date: '28/03/2020 ',
                         hour:' 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 ', 
+                        date: '28/03/2020 ',
                         hour:' 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
@@ -97,13 +101,13 @@ var app = new Vue({
                 avatar: '_4',
                 messages: [
                     {
-                        date: '10/01/2020 ', 
+                        date: '10/01/2020 ',
                         hour:' 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 ', 
+                        date: '10/01/2020 ',
                         hour:' 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -115,13 +119,13 @@ var app = new Vue({
                 avatar: '_5',
                 messages: [
                     {
-                        date: '10/01/2020 ', 
+                        date: '10/01/2020 ',
                         hour:' 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 ', 
+                        date: '10/01/2020 ',
                         hour:' 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -133,19 +137,19 @@ var app = new Vue({
                 avatar: '_6',
                 messages: [
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -157,13 +161,13 @@ var app = new Vue({
                 avatar: '_7',
                 messages: [
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -175,19 +179,19 @@ var app = new Vue({
                 avatar: '_8',
                 messages: [
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020', 
+                        date: '10/01/2020',
                         hour:' 15:51:00',
                         message: 'OK!!',
                         status: 'received'
@@ -195,23 +199,10 @@ var app = new Vue({
                 ],
             }
         ]
-    }, 
+    },
     mounted () {
         let listContact = document.querySelectorAll('li');
-        listContact[this.activeChatIndex].classList.add('selected-contact') ;
-    // calcolo tipo dispostivo
-        let screenWidth = window.screen.width;
-        let aside = document.querySelector('aside')
-        let chat = document.getElementById('message-app') 
-        if (screenWidth < 578) {
-            this.screenType = 'mobile';
-            aside.classList.remove('d-none');
-            chat.classList.add('d-none');
-        } else if (screenWidth > 578) {
-            this.screenType = 'desktop';
-            aside.classList.remove('d-none');
-            document.getElementById('message-app').classList.remove('d-none');
-        }
+        listContact[this.activeChatIndex].classList.add('selected-contact');
     },
     computed: {
     // Funzione Filtraggio
@@ -220,40 +211,60 @@ var app = new Vue({
             let str = this.searchInput.trim();
             if (str !== '') {
                 for (let i = 0; i < this.contacts.length; i++) {
-                    if (!this.contacts[i].name.includes(str)) {
+                    if (!this.contacts[i].name.toLowerCase().includes(str.toLowerCase())) {
                         listContact[i].classList.add('d-none')
                         console.log(`L'input di ricerca è ${str}`)
                         console.log('contatto i =' + ' ' + listContact[i]);
                         console.log(` La stringa ${str} NON è contenuta in ${this.contacts[i].name}`)
+                        this.noCnt = true;
                     } else {
                         console.log(` La stringa ${str} è contenuta in ${this.contacts[i].name}`)
+                        this.noCnt = false;
                     }
                 }
             } else if (str == '') {
                 for (let x = 0; x < this.contacts.length; x++) {
-                    listContact[x].classList.remove('d-none')
+                    listContact[x].classList.remove('d-none');
+                    this.noCnt = false;
                 }
             }
             return str;
-        }
-        /*
-    //Funzione recupero ultimo messaggio default
-        getLastMessageDefault() {},
-    //Funzione recupero data ultimo messaggio default
-        getLastMessageDefaultTime() {},
-    //Funzione recupero ultimo messaggio
-        getLastMessageInfo() {},
+        },
+    // Controllo tipo device
+        deviceScreenType () {
+            let screenWidth = window.screen.width;
+            if (screenWidth < 578) {
+                this.screenType = 'mobile';
+            } else if (screenWidth > 578) {
+                this.screenType = 'desktop';
+            }
+            return this.screenType;
+        },
+        bypassMQ () {
+            let aside = document.querySelector('aside');
+            let chat = document.getElementById('message-app');
+            if (this.screenType == 'mobile') {
+                aside.classList.remove('d-none');
+                chat.classList.add('d-none');
+            } else {
+                aside.classList.remove('d-none');
+                chat.classList.remove('d-none');
+            }
+        },
+
     //Funzione recupero data ultimo messaggio
-        getLastMessageTime() {}
-        */
+        // getLastMessageTime() {
+        // }
+
     },
     methods: {
         moveActive(index) {
             let listContact = document.querySelectorAll('li');
             listContact[this.activeChatIndex].classList.remove('selected-contact');
+            listContact[this.activeChatIndex].classList.remove('selected-contact-alt');
             this.activeChatIndex = index;
             listContact[this.activeChatIndex].classList.add('selected-contact');
-            console.log(`Il contatto attivo è: ${this.activeChatIndex}`)
+            console.log(`Il contatto attivo è: ${this.activeChatIndex}`);
         },
     // Funzione nascondi notifica
         hide() {
@@ -300,22 +311,72 @@ var app = new Vue({
                         message: 'Ok',
                         status: 'received'
                     }
-                    this.lastMessage = newR.message,
-                        this.contacts[this.activeChatIndex].messages.push(newR)
+                    this.lastMessage = newR.message;
+                    this.contacts[this.activeChatIndex].messages.push(newR);
                 }, 2000)
             }
         },
-        // Funzione Recupero ora e data attuale
+    // Funzione Recupero ora e data attuale
         getTimeActual() {
-            this.lastMessageTime = DateTime.now().toFormat('HH:mm:ss')
-            this.lastMessageDate = DateTime.now().toFormat('dd/LL/y')
+            this.lastMessageTime = DateTime.now().toFormat('HH:mm:ss');
+            this.lastMessageDate = DateTime.now().toFormat('dd/LL/y');
         },
-    // Funzione di visualizzazione chat selezionata mobile
+    // Cambio  tema
+        enableDarkMode () {
+            document.querySelector('#message-app-chat').classList.toggle('dark-mode-bg');
+            document.querySelector('aside').classList.toggle('dark-mode');
+            document.querySelector('#app-header').classList.toggle('dark-mode');
+            document.querySelector('header').classList.toggle('dark-mode');
+            document.querySelector('footer').classList.toggle('dark-mode');
+            document.querySelector('#search-bar-container').classList.toggle('dark-mode');
+        // Cambio Colore Icone
+            let icons = document.querySelectorAll('i');
+            for (let i = 0; i < icons.length; i++) {
+                icons[i].classList.toggle('dark-mode-icon');
+            }
+        // Icone tema
+            document.querySelector('.fa-circle-half-stroke').classList.toggle('d-none');
+            document.querySelector('.fa-moon').classList.toggle('d-none');
+        },
+    // Funzione di visualizzazione chat / contatti mobile
         viewMessage () {
             if (this.screenType == 'mobile') {
-                document.querySelector('aside').classList.add('d-none');
-                document.querySelector('#message-app').classList.add('override-MQ')
+                document.querySelector('aside').classList.toggle('d-none');
+                document.querySelector('#message-app').classList.toggle('d-none');
             }
-        }
+        },
+    // Pulsante Ricarica pagina
+        reload () {
+            location.reload();
+        },
+
+    // Cancella Messaggio ---
+        // deleteMsg () {
+            // this.contacts[this.activeChatIndex].messages.splice(this.contacts.messages[this.activeChatIndex]);
+        // },
+
+    // Svuota Chat
+        deleteChat () {
+            this.contacts[this.activeChatIndex].messages = [];
+            this.chatEmpty = true;
+            this.ctrHiddenHD();
+        },
+    // Toggle menu header chat
+        ctrHiddenHD() {
+            if (this.isHiddenHD == true) {
+                this.isHiddenHD = false;
+            } else {
+                this.isHiddenHD = true;
+            }
+        },
+
+    // Toggle menu messaggi
+        // ctrHidM() {
+        //     if (this.isHidden == true) {
+        //         this.isHidden = false;
+        //     } else {
+        //         this.isHidden = true;
+        //     }
+        // }
     }
 })
